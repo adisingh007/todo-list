371e291adfe0f2b197d9e877f0a1f34f0f20b7c5
Aditya Singh <adipratapsingh.aps@gmail.com>
2021-12-05 00:08:09 +0530

    Changed TODO from a single line of test to a TOdo object which has a unique UUID and a completed flag

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
 
 const todoList = [];
 
+class Todo {
+    constructor(title, description = "", completed = false) {
+        this.uuid = uuid.v1();
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+}
+
 app.use(express.json());
 
 app.get('/', (_, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.send('<h1>Thakur Software Services</h1><br>✅TODO List!');
+    res.send('Thakur Software Services - ✅TODO List!');
 });
 
-app.post('/todo', (req, res) => {
-    const todo = req.body.task;
+app.put('/todo', (req, res) => {
+    const task = req.body.task;
 
-    if(!todo) {
+    if(!task) {
         res.status(400);
-        res.setHeader('Content-Type', 'text/html');
-        res.send("<div align='center'>No task provided</div>");
-    } else{
-        todoList.push(req.body.task);
+        res.send("No task provided");
+    } else if(!task.title) {
+        res.status(400);
+        res.send("No task title provided");
+    } else {
+        const todo = new Todo(task.title, task.description);
+        todoList.push(todo);
         res.status(200);
         res.setHeader('Content-Type', 'application/json');
         res.send({
-            taskAdded: todo,
+            task: todo,
         });
     }
 });
@@ -31,6 +43,25 @@ app.get('/todo', (_, res) => {
     res.status(200);
     res.setHeader('Content-Type', 'application/json');
     res.send(todoList);
+});
+
+app.post('/todo/:uuid', (req, res) => {
+    const todo = todoList.find(todo => todo.uuid === req.params.uuid);
+    if(todo) {
+        if(!todo.completed) {
+            todo.completed = true;
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(todo);
+        } else {
+            res.status(404);
+            res.send(`Task ${req.params.uuid} already marked complete!`);
+        }
+    } else {
+        res.status(404);
+        res.setHeader('Content-Type', 'text/html');
+        res.send(`Task ${req.params.uuid} not found`);
+    }
 });
 
 const port = 3000;
