e41ee59741f7c45587aa2069ff4a38cec9c555a0
Aditya Singh <adipratapsingh.aps@gmail.com>
2021-12-05 00:57:49 +0530

    Removed 2 endpoints to check and uncheck task and merged them. POsting only 1 task at a time.

--- index.js
+++ index.js
@@ -20,28 +20,23 @@ app.get('/', (_, res) => {
     res.send('Thakur Software Services - ✅TODO List!');
 });
 
-app.put('/todo', (req, res) => {
-    const tasks = req.body.tasks;
-    if(!tasks) {
+app.post('/todo', (req, res) => {
+    const task = req.body.task;
+    if(!task) {
         res.status(400);
-        res.send('Tasks not provided');
+        res.send('Task not provided');
     } else {
-        const response = {
-            success: [],
-            error: []
-        };
-        tasks.forEach(task => {
-            if(task.title) {
-                const todo = new Todo(task.title, task.description);
-                todoList.push(todo);
-                response.success.push(todo);
-            } else {
-                response.error.push(task);
-            }
-        });
-        res.status(200);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(response);
+
+        if(task.title) {
+            const todo = new Todo(task.title, task.description);
+            todoList.push(todo);
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(todo);
+        } else {
+            res.status(400);
+            res.send('Title not provided!');
+        }
     }
 });
 
@@ -51,39 +46,16 @@ app.get('/todo', (_, res) => {
     res.send(todoList);
 });
 
-app.post('/todo/markComplete/:uuid', (req, res) => {
+app.patch('/todo/:uuid', (req, res) => {
     const todo = todoList.find(todo => todo.uuid === req.params.uuid);
     if(todo) {
-        if(!todo.completed) {
-            todo.completed = true;
-            res.status(200);
-            res.setHeader('Content-Type', 'application/json');
-            res.send(todo);
-        } else {
-            res.status(409);
-            res.send(`Task ${req.params.uuid} already marked complete!`);
-        }
+        todo.completed = !todo.completed;
+        res.status(200);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(todo);
     } else {
         res.status(404);
         res.send(`Task ${req.params.uuid} not found`);
-    }
-});
-
-app.post('/todo/markInomplete/:uuid', (req, res) => {
-    const todo = todoList.find(todo => todo.uuid === req.params.uuid);
-    if(todo) {
-        if(todo.completed) {
-            todo.completed = false;
-            res.status(200);
-            res.setHeader('Content-Type', 'application/json');
-            res.send(todo);
-        } else {
-            res.status(409);
-            res.send(`Task ${req.params.uuid} already marked incomplete!`);
-        }
-    } else {
-        res.status(404);
-        res.send(`Task ${req.params.uuid} not found!`);
     }
 });
 
