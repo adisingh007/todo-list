8f8eb3c98f685685802285e2e340572a2fbdc50c
Aditya Singh <adipratapsingh.aps@gmail.com>
2021-12-06 02:45:35 +0530

    Demonstrated the beauty of dependency injection. Integrated mongodb with just addition of 1 file with mongo-specific functions and changes only in index.js where we are injecting mongo object to server.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const server = require('./server/server');
-const todoList = require('./models/in-memory-todo-list/in-memory-todo-list');
+const todoList = require('./models/mongodb-todo-list/mongodb-todo-list');
 
 const port = process.env.PORT || 3000;
 server.setup(todoList).start(port);
---
+++ src/models/mongodb-todo-list/mongodb-todo-list.js
@@ -0,0 +1,55 @@
+const Task = require('../task');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'todo-list';
+const collectionName = 'tasks';
+
+async function useDB(command, collectionName) {
+    const mongoClient = new MongoClient(url);
+    try {
+        await mongoClient.connect();
+        const db = mongoClient.db(dbName);
+        const collection = db.collection(collectionName);
+        return await command(collection);
+    } catch(ex) {
+        console.error(ex);
+    } finally {
+        mongoClient.close();
+    }
+}
+
+const addTask = async (title, description = '') => {
+    return await useDB(async collection => await collection.insertOne(new Task(title, description)), collectionName);
+};
+
+const removeTask = async (uuid) => {
+    const removedTask = await getTask(uuid);
+    await useDB(async collection => await collection.deleteOne({uuid}), collectionName);
+    return removedTask;
+};
+
+const getTask = async (uuid) => {
+    return await useDB(async collection => await collection.findOne({uuid}), collectionName);
+};
+
+const hasTask = async (uuid) => {
+    return await useDB(async collection => await collection.findOne({uuid}), collectionName);
+};
+
+const toggleTask = async (uuid) => {
+    const task = await getTask(uuid);
+    await useDB(async collection => await collection.updateOne({uuid}, {'$set': {completed: !task.completed}}), collectionName);
+};
+
+const getTasks =  async () => {
+    return await useDB(async collection => await collection.find({}).toArray(), collectionName);
+};
+
+module.exports = {
+    addTask,
+    removeTask,
+    getTask,
+    hasTask,
+    toggleTask,
+    getTasks,
+}
