4defe40be70afa3f36a9fe85cf235e484873fb50
Aditya Singh <adipratapsingh.aps@gmail.com>
2021-12-05 23:09:11 +0530

    Moved in-memory-todo-list to its own folder.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const server = require('./server');
-const todoList = require('./models/in-memory-todo-list');
+const todoList = require('./models/in-memory-todo-list/in-memory-todo-list');
 
 const port = process.env.PORT || 3000;
 server.setup(todoList).start(port);
